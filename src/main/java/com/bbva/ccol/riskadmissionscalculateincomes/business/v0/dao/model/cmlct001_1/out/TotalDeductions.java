c29f659a791a5530eb121bdc68dcb1e29045f65c
andresfelipe.buitrago.gualter <andresfelipe.buitrago.gualter@bbva.com>
2024-12-09 01:37:47 -0500

    "nuevo servicio"

---
+++ src/main/java/com/bbva/ccol/riskadmissionscalculateincomes/business/v0/dao/model/cmlct001_1/out/TotalDeductions.java
@@ -0,0 +1,45 @@
+package com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao.model.cmlct001_1.out;
+
+import com.bbva.jee.arq.spring.core.host.Campo;
+import com.bbva.jee.arq.spring.core.host.FilaCampoTabular;
+import com.bbva.jee.arq.spring.core.host.TipoCampo;
+
+/**
+ * <p>Bean fila para el campo tabular <code>totalDeductions</code>, utilizado por la clase <code>Data</code></p>
+ * 
+ * @see Data
+ * 
+ * @author Arquitectura Spring BBVA
+ */
+@FilaCampoTabular
+public class TotalDeductions {
+	
+	/**
+	 * <p>Campo <code>amount</code>, &iacute;ndice: <code>1</code>, tipo: <code>ENTERO</code>
+	 */
+	@Campo(indice = 1, nombre = "amount", tipo = TipoCampo.ENTERO, longitudMaxima = 15, signo = true, obligatorio = true)
+	private long amount;
+	
+	/**
+	 * <p>Campo <code>currency</code>, &iacute;ndice: <code>2</code>, tipo: <code>ALFANUMERICO</code>
+	 */
+	@Campo(indice = 2, nombre = "currency", tipo = TipoCampo.ALFANUMERICO, longitudMaxima = 255, signo = true, obligatorio = true)
+	private String currency;
+	
+
+	public long getAmount() {
+	    return amount;
+	}
+
+	public void setAmount(long amount) {
+    	this.amount = amount;
+	}
+
+	public String getCurrency() {
+	    return currency;
+	}
+
+	public void setCurrency(String currency) {
+    	this.currency = currency;
+	}
+}
