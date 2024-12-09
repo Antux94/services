336decc34a92d7afd8222eef6f12ce5e11824f05
andresfelipe.buitrago.gualter <andresfelipe.buitrago.gualter@bbva.com>
2024-12-09 11:06:02 -0500

    "nuevo servicio"

---
+++ src/main/java/com/bbva/ccol/riskadmissionscalculateincomes/business/v0/dao/IRiskAdmissionsCalculateIncomesDAO.java
@@ -0,0 +1,10 @@
+//package com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao;
+//
+//import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dto.BCalculate;
+//import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dto.BBody;
+//
+//public interface IRiskAdmissionsCalculateIncomesDAO {
+//    //BData createData(BCalculate bCalculate);
+//    void createData(BBody bBody);
+//
+//}
