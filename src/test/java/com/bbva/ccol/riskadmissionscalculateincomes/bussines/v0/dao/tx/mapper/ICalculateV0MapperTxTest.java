95634a81ea8de246a498f461552df140c6428b65
andresfelipe.buitrago.gualter <andresfelipe.buitrago.gualter@bbva.com>
2024-12-09 00:56:49 -0500

    "nuevo servicio"

---
+++ src/test/java/com/bbva/ccol/riskadmissionscalculateincomes/bussines/v0/dao/tx/mapper/ICalculateV0MapperTxTest.java
@@ -0,0 +1,122 @@
+
+package com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao.tx.mapper;
+
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao.model.cmlct001_1.in.IdentityDocument;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao.model.cmlct001_1.in.InformationSources;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao.model.cmlct001_1.in.LaboralInformation;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao.model.cmlct001_1.in.Person;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao.model.cmlct001_1.in.Product;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dao.model.cmlct001_1.in.RequestTransactionCmlct001_1;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dto.BBody;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dto.BIdentityDocument;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dto.BInformationSources;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dto.BLaboralInformation;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dto.BPerson;
+import com.bbva.ccol.riskadmissionscalculateincomes.business.v0.dto.BProduct;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class ICalculateV0MapperTxTest {
+
+    private ICalculateV0MapperTx mapper = Mappers.getMapper(ICalculateV0MapperTx.class);
+
+    private DummyMock dummyMock;
+
+    @Before
+    public void setUp() {
+        dummyMock = new DummyMock();
+    }
+
+    @Test
+    public void testMapInApxData() {
+        BBody bBody = dummyMock.getBBody();
+        RequestTransactionCmlct001_1 request = mapper.mapInApxData(bBody);
+
+        Assert.assertNotNull(request);
+
+        // Person Assertions
+        Person person = request.getPerson();
+        Assert.assertNotNull(person);
+        Assert.assertEquals(bBody.getPerson().getFirstName(), person.getFirstName());
+        Assert.assertEquals(bBody.getPerson().getMiddleName(), person.getMiddleName());
+        Assert.assertEquals(bBody.getPerson().getLastName(), person.getLastName());
+
+        // IdentityDocument Assertions
+        IdentityDocument identityDocument = person.getIdentityDocument();
+        Assert.assertNotNull(identityDocument);
+        Assert.assertEquals(bBody.getPerson().getIdentityDocument().getDocumentType(), identityDocument.getDocumentType());
+        Assert.assertEquals(bBody.getPerson().getIdentityDocument().getDocumentNumber(), identityDocument.getDocumentNumber());
+
+        // LaboralInformation Assertions
+        LaboralInformation laboralInformation = person.getLaboralInformation();
+        Assert.assertNotNull(laboralInformation);
+        Assert.assertEquals(bBody.getPerson().getLaboralInformation().getCompanyId(), laboralInformation.getCompanyId());
+        Assert.assertEquals(bBody.getPerson().getLaboralInformation().getDeclaratedIncome().longValue(), laboralInformation.getDeclaratedIncome());
+
+
+        // InformationSources Assertions
+        List<BInformationSources> bInformationSourcesList = bBody.getInformationSources();
+        List<InformationSources> informationSourcesList = request.getInformationSources();
+        Assert.assertNotNull(informationSourcesList);
+        Assert.assertEquals(bInformationSourcesList.size(), informationSourcesList.size());
+        for (int i = 0; i < bInformationSourcesList.size(); i++) {
+            Assert.assertEquals(bInformationSourcesList.get(i).getId(), informationSourcesList.get(i).getId());
+            Assert.assertEquals(String.valueOf(bInformationSourcesList.get(i).getIsConsulted()), informationSourcesList.get(i).getIsConsulted());
+        }
+
+        // Product Assertions
+        Product product = request.getProduct();
+        Assert.assertNotNull(product);
+        Assert.assertEquals(bBody.getProduct().getId(), product.getId());
+        Assert.assertEquals(bBody.getProduct().getName(), product.getName());
+    }
+
+
+    private class DummyMock {
+
+        public BBody getBBody() {
+            BBody bBody = new BBody();
+
+            BPerson bPerson = new BPerson();
+            bPerson.setFirstName("Christian");
+            bPerson.setMiddleName("David");
+            bPerson.setLastName("Rojas");
+
+            BIdentityDocument bIdentityDocument = new BIdentityDocument();
+            bIdentityDocument.setDocumentType("CC");
+            bIdentityDocument.setDocumentNumber("1235489345");
+            bPerson.setIdentityDocument(bIdentityDocument);
+
+            BLaboralInformation bLaboralInformation = new BLaboralInformation();
+            bLaboralInformation.setCompanyId("800900231-5");
+            bLaboralInformation.setDeclaratedIncome(Double.valueOf(5000000));
+            bPerson.setLaboralInformation(bLaboralInformation);
+
+            bBody.setPerson(bPerson);
+
+            List<BInformationSources> bInformationSourcesList = new ArrayList<>();
+            BInformationSources bInformationSources = new BInformationSources();
+            bInformationSources.setId("SOI");
+            bInformationSources.setIsConsulted(Boolean.FALSE);
+            bInformationSourcesList.add(bInformationSources);
+            bBody.setInformationSources(bInformationSourcesList);
+
+            BProduct bProduct = new BProduct();
+            bProduct.setId("12");
+            bProduct.setName("hipotecas");
+            bBody.setProduct(bProduct);
+
+            return bBody;
+        }
+    }
+}
